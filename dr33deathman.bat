cf746058f08e97f551c9a11212e8f76e7568a422
Dr33Deathman <54439558+Dr33Deathman@users.noreply.github.com>
2020-03-27 22:43:30 +0600

    Windows Install and run
    For intalling virtual environment, pygame and download labworks

---
+++ dr33deathman.bat
@@ -0,0 +1,10 @@
+pip install virtualenv
+python -m venv env
+curl -o ex1.py https://cdn.discordapp.com/attachments/689836543526830122/693091688675016722/ex1.py
+curl -o ex2.py https://cdn.discordapp.com/attachments/689836543526830122/693091689287385138/ex2.py
+curl -o ex3.py https://cdn.discordapp.com/attachments/689836543526830122/693091674959773696/ex3.py
+curl -o ex4.py https://cdn.discordapp.com/attachments/689836543526830122/693091677837197312/ex4.py
+curl -o ex5.py https://cdn.discordapp.com/attachments/689836543526830122/693091680932331590/ex5.py
+curl -o ex6.py https://cdn.discordapp.com/attachments/689836543526830122/693091682849128548/ex6.py
+echo open terminal and run "pip install pygame" in it\nEnjoy this labs c: > README.txt
+.\env\Scripts\activate
