ae03c4327e86dbb332041a2cc367e81d7723d550
Dr33Deathman <54439558+Dr33Deathman@users.noreply.github.com>
2020-04-02 22:08:15 +0600

    Create labwork8.py

---
+++ labwork8/labwork8.py
@@ -0,0 +1,60 @@
+#                   Lab 8
+import pygame 
+
+running = True # Flag for infinite cicle
+pygame.init() # Initialize video environment for pygame
+
+screen=pygame.display.set_mode((1080,720))   # use your own screen size
+
+screen_size = screen.get_size()              # get your screen size
+background = pygame.Surface(screen_size)     # use that screen size for create surface
+background.fill((255, 255, 255))             # fill the background black 
+background = background.convert()            # just copy new background into new variable to easy access it
+
+circle_x = screen_size[0]//2 # start in center of your screen
+circle_y = screen_size[1]//2        
+
+FPS = 60 # desired framerate in frames per second. 
+clock = pygame.time.Clock() # Initializing clock for usin specific frame 
+
+while running:  # infinite cicle that make windown does't close
+
+    milliseconds = clock.tick(FPS) # do not go faster than this framerate
+
+    for event in pygame.event.get():     # iterate all events
+        if event.type == pygame.QUIT:    # if we close window with our application
+            running = False              # make your program turn off
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False     # make your program turn off, when you press ESCAPE Button
+
+
+            # version with 1 press == 1 move
+
+            # if event.key == pygame.K_RIGHT and circle_x + 20 < screen_size[0]:   # Checking pressed button and screen borders for moving ball
+            #     circle_x += 20        # move to the right
+            # if event.key == pygame.K_LEFT and circle_x > 20:
+            #     circle_x -= 20        # move to the left
+            # if event.key == pygame.K_UP and circle_y > 20:
+            #     circle_y -= 20        # goin up
+            # if event.key == pygame.K_DOWN and circle_y + 20 < screen_size[1]: 
+            #     circle_y += 20        # goin down
+
+
+    # version whith holding pressed key
+
+    keys = pygame.key.get_pressed() # grab all pressed keys
+    if keys[pygame.K_RIGHT] and circle_x + 20 < screen_size[0]: # Checking pressed button and screen borders for moving ball
+        circle_x += 20      # move to the right
+    if keys[pygame.K_LEFT] and circle_x > 20:
+        circle_x -= 20      # move to the left
+    if keys[pygame.K_UP] and circle_y > 20:
+        circle_y -= 20      # goin up
+    if keys[pygame.K_DOWN] and circle_y + 20 < screen_size[1]:
+        circle_y += 20      # goin down
+
+
+    screen.blit(background, (0, 0)) # draw our background
+    pygame.draw.circle(screen, (176, 26, 26), (circle_x, circle_y), 25)  # draw our red circle 
+    pygame.display.flip()  # Update picture on your screen
